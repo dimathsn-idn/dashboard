03348cae3da040dd5404bab76c2ce1ddb39299f9
dimathsn-idn <dimathsn@gmail.com>
2026-07-09 15:00:19 +0700

    Add files via upload

---
+++ lapkeu-17an-p2/lapkeu_17an_p2.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+st.set_page_config(page_title="Dashboard", layout="wide")
+st.title(":blue[Laporan Keuangan Kegiatan 17an Persatuan 2 KDW]", text_alignment="center")
+
+conn_uang = st.connection("gsheets_uang", type=GSheetsConnection)
+data_uang = conn_uang.read()
+st.badge(f"Data Masuk Terakhir: {data_uang.loc[data_uang.index[-1], 'TANGGAL']}", icon=":material/database:", color="blue")
+st.badge(f"SALDO: {data_uang.loc[data_uang.index[-1], 'SALDO']}", icon=":material/money:", color="blue")
+data_transaksi = data_uang.melt(value_vars=['PEMASUKAN', 'PENGELUARAN'], var_name='TRANSAKSI', value_name='NILAI TRANSAKSI')
+transaksi_grafik = px.bar(data_transaksi, x='TRANSAKSI', y='NILAI TRANSAKSI', color='TRANSAKSI', text_auto=True)
+transaksi_grafik.update_yaxes(tickformat=",")
+transaksi_grafik.update_layout(yaxis_title="TOTAL")
+transaksi_grafik.update_traces(width=0.5, textposition='outside', textfont_size=20, textfont_weight=500, textfont_color='blue')
+st.plotly_chart(transaksi_grafik)
+with st.expander("Detil Transaksi"):
+    st.dataframe(data_uang)
